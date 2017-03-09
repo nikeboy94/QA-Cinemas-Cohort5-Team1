2edb9e03fa7dd5aab327411f4c0b27dcb6f85409
omar-f <o.fahmy@outlook.com>
2017-03-09 15:51:33 +0000

    QA-57 #comment Improved backend functionality and fixed some errors

--- src/main/java/com/qa/cinema/persistence/Ticket.java
+++ src/main/java/com/qa/cinema/persistence/Ticket.java
@@ -97,9 +97,5 @@ public class Ticket {
 		this.showing = updatedTicket.getShowing();
 	}
 	
-	
-
-	
-	
 
 }
--- src/main/java/com/qa/cinema/service/DBTicketService.java
+++ src/main/java/com/qa/cinema/service/DBTicketService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.apache.log4j.Logger;
+
 import com.qa.cinema.persistence.Ticket;
 import com.qa.cinema.persistence.Showing;
 import com.qa.cinema.util.JSONUtil;
@@ -28,12 +30,16 @@ import com.qa.cinema.util.JSONUtil;
 @Default
 public class DBTicketService implements TicketService {
 
+	final static Logger logger = Logger.getLogger(DBTicketService.class);
+	
 	@PersistenceContext(unitName = "primary")
 	private EntityManager manager;
 	
 	@Inject
 	private JSONUtil util;
 	
+	@Inject
+	private ShowingService showingService;
 	
 	@Override
 	public String getUserTickets(String email) {
@@ -52,7 +58,20 @@ public class DBTicketService implements TicketService {
 
 	@Override
 	public String updateTicket(Long ticketId, String newTicket) {
+		logger.info("DBTICKETSERVICE: Entered updateTicket method. About to get updatedTicket object");
 		Ticket updatedTicket = util.getObjectForJSON(newTicket, Ticket.class);
+		
+		logger.info("DBTICKETSERVICE: About to get updatedShowing object");
+		Showing updatedShowing = getShowing(updatedTicket.getShowing().getShowingId());
+		logger.info("DBTICKETSERVICE: updatedShowing created");
+		
+		if(updatedShowing == null) {
+			return "{\"messege\": \"Showing not found\"}";
+		}
+	
+		logger.info("DBTICKETSERVICE: About to call updatedTicket.setShowing");
+		updatedTicket.setShowing(updatedShowing);
+		
 		Ticket ticketInDB = findTicket(ticketId);
 		if (ticketInDB != null){
 			ticketInDB.updateField(updatedTicket);
@@ -88,8 +107,29 @@ public class DBTicketService implements TicketService {
 		return "{\"availableTickets\": \"" +availableTickets +"\"}";
 	}
 	
-	public Ticket findTicket(Long ticketId) {
+	private Ticket findTicket(Long ticketId) {
 		return manager.find(Ticket.class, ticketId);
+	}
+	
+	private Showing getShowing(Long showingId) {
+
+		logger.info("DBTICKETSERVICE - getShowing. About to create string from showing service");
+		String allShowingsJSON = showingService.getAllShowings();
+		
+		logger.info("DBTICKETSERVICE - getShowing. About to make a collection of allShowings");
+		Showing[]allShowings = (Showing[]) util.getObjectForJSON(showingService.getAllShowings(), Showing[].class);
+		
+		logger.info("DBTICKETSERVICE - getShowing. About to loop over allShowings");
+		for(Showing aShowing : allShowings) {
+			
+			logger.info("DBTICKETSERVICE - getShowing. About to call getShowingId() and compare against showing");
+			if(aShowing.getShowingId().equals(showingId)) {
+				logger.info("DBTICKETSERVICE - getShowing. Correct showing found, about to return aShowing");
+				return aShowing;
+			}
+		}
+		logger.info("DBTICKETSERVICE - getShowing. Loop finished, about to return null");
+		return null;
 	}
 
 }
