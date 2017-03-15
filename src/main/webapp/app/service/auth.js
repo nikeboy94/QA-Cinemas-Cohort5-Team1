b0210a56962bb3a779d5d4c1f5da8117c6010419
Saron Arulneshan <sarulneshan42@gmail.com>
2017-03-15 17:31:18 +0000

    QA-55

--- src/main/webapp/app/service/auth.js
+++ src/main/webapp/app/service/auth.js
@@ -29,12 +29,13 @@ function($http, $rootScope, $window, $cookieStore, Session, AUTH_EVENTS, userDAL
         });
 		
 	};
-    authService.setTicketQuantity = function(qty) {
-        if ($rootScope.globals.currentUser == undefined) {
+
+	authService.setTicketQuantity = function(qty) {
+		if ($rootScope.globals.currentUser == undefined) {
             $rootScope.globals.currentUser = {};
-        }
-        $rootScope.globals.currentUser.ticketQuantity = qty;
-    };
+		}
+		$rootScope.globals.currentUser.ticketQuantity = qty;
+	};
 
 	authService.setCredentials = function() {
         $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.email;
@@ -47,9 +48,28 @@ function($http, $rootScope, $window, $cookieStore, Session, AUTH_EVENTS, userDAL
         $http.defaults.headers.common.Authorization = 'Basic ';
     };
 
-    authService.getTicketQuantity = function() {
-    	return $rootScope.globals.currentUser.ticketQuantity;
-	}
+
+    authService.addOrder = function(ticketArray) {
+    	$rootScope.globals.currentUser.order = ticketArray;
+	};
+
+    authService.getOrder = function() {
+    	return $rootScope.globals.currentUser.order;
+	};
+
+
+
+    authService.addCard = function(card) {
+    	if ($rootScope.globals.currentUser == undefined) {
+    		$rootScope.globals.currentUser = {};
+		}
+    	$rootScope.globals.currentUser.card = card;
+	};
+
+    authService.getCard = function() {
+        return $rootScope.globals.currentUser.card;
+    };
+
 
     authService.setSeats = function(seats) {
     	if($rootScope.globals.currentUser == undefined) {
@@ -60,6 +80,7 @@ function($http, $rootScope, $window, $cookieStore, Session, AUTH_EVENTS, userDAL
 
 	authService.getSeats = function() {
     	return $rootScope.globals.currentUser.seats;
+
 	};
 
 	//check if the user is authenticated
@@ -84,7 +105,7 @@ function($http, $rootScope, $window, $cookieStore, Session, AUTH_EVENTS, userDAL
 		$window.sessionStorage.removeItem("userInfo");
 		$rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
     $rootScope.globals.currentUser.email = undefined;
-  }
+  };
 
 	return authService;
 } ]);
