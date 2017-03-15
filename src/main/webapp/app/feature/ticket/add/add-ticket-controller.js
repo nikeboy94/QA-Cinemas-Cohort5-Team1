040a47d105ced783827dd94304f0c13415b24d2b
omar-f <o.fahmy@outlook.com>
2017-03-15 22:17:21 +0000

    QA-144 #comment Implemented multiple ticket types on the front end

--- src/main/webapp/app/feature/ticket/add/add-ticket-controller.js
+++ src/main/webapp/app/feature/ticket/add/add-ticket-controller.js
@@ -1,64 +1,83 @@
-(function() {
+(function () {
 
-    var AddTicketController = function(ticketDal, Auth, $state, movieDal, showingDal ) {
+    var AddTicketController = function (ticketDal, Auth, $state, movieDal, showingDal) {
         var vm = this;
 
         vm.ticketArray = [];
 
-        this.addTicket = function(ticket, qty) {
-            Auth.setTicketQuantity(qty);
-           // $state.go('dashboard');
-            var myId = '1';
-            ticketDal.getPrice(myId).then(function(results) {
-                ticket.price = results.price;
-            }), function(error) {
+        this.addTicket = function (ticket, adultQty, childQty) {
+
+            var addChildTickets = function () {
+                ticketDal.getPrice(ticket.showing.showingId, 'CHILD').then(function(result) {
+                    ticketDalSuccess(result, childQty, childTicket);
+                    Auth.addOrder(vm.ticketArray);
+                }), function(error) {
+                    ticketDalFailure(error);
+                }
+            }
+            
+            var ticketDalSuccess = function (result, qty, specificTicket) {
+                specificTicket.price = result.price;
+                for (var i = 0; i < qty; i++) {
+                    vm.ticketArray.push(specificTicket);
+                }
+            }
+
+            var ticketDalFailure = function(error) {
                 vm.error = true;
-                vm.errorMessage = error;
+                vm.errorMessage = errorMessage;
+            }
+
+            var initNewTicket = function() {
+                var newTicket = {};
+                newTicket.orderId = ticket.orderId;
+                newTicket.seat = ticket.seat;
+                newTicket.showing = ticket.showing;
+                newTicket.user = ticket.user;
+
+                return newTicket;
             };
 
-            ticket.orderId=new Date().getTime()
-            for (var i = 0; i < qty; i++) {
-                vm.ticketArray.push(ticket);
-            }
-            console.log(vm.ticketArray);
-            Auth.addOrder(vm.ticketArray);
+            ticket.orderId = new Date().getTime();
+            Auth.setTicketQuantity(adultQty + childQty);
 
+            var childTicket = initNewTicket();
+            var adultTicket = initNewTicket();
+            adultTicket.ticketType = 'ADULT';
+            childTicket.ticketType = 'CHILD';
+
+            ticketDal.getPrice(ticket.showing.showingId, 'ADULT').then(function(result) {
+                ticketDalSuccess(result, adultQty, adultTicket);
+                addChildTickets();
+            }), function(error) {
+                ticketDalFailure(error);
+            }
+
+            // $state.go('dashboard');
+
+        
         };
 
-        vm.init = function(){
+        vm.init = function () {
 
-            movieDal.getMovies().then(function(results){
-                vm.movieList=results;
-            }), function(error) {
+            movieDal.getMovies().then(function (result) {
+                vm.movieList = result;
+            }), function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
             }
         };
         vm.init();
 
-        vm.getShowingsById = function(movieId) {
-            showingDal.getShowingByMovie(movieId).then(function(results){
-                vm.movieShowingList=results;
-            }), function(error){
+        vm.getShowingsById = function (movieId) {
+            showingDal.getShowingByMovie(movieId).then(function (result) {
+                vm.movieShowingList = result;
+            }), function (error) {
                 alert(error);
                 vm.error = true;
                 vm.errorMessage = error;
             }
         };
-
-        /*
-        vm.addTicket = function(ticketToAdd) {
-
-            ticketDal.addTicket(ticketToAdd).then(function (results) {
-                vm.ticketAddMessage = results;
-            }, function (error) {
-                vm.error=true;
-                vm.errorMessage = error;
-
-            });
-
-
-        }; */
     };
 
     angular.module('movieApp').controller('addTicketController', ['ticketDal', 'Auth', '$state', 'movieDal', 'showingDal', AddTicketController]);
