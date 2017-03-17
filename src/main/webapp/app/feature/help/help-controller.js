9755351e4d1448d58e9869af6ea0b81214ddf448
Saron Arulneshan <sarulneshan42@gmail.com>
2017-03-17 14:25:15 +0000

    QA=55 #comment Help completed

--- src/main/webapp/app/feature/help/help-controller.js
+++ src/main/webapp/app/feature/help/help-controller.js
@@ -2,11 +2,14 @@
     var HelpController = function ($state) {
         var vm = this;
 
-        vm.contactus = function(){
+        vm.contactus = function () {
             $state.go("contactus");
         }
 
+        vm.glyphicon = function(){
+            this.class.removeClass()
+        }
 
     };
-angular.module('movieApp').controller('helpController', ['$state', HelpController]);
+    angular.module('movieApp').controller('helpController', ['$state', HelpController]);
 }());
