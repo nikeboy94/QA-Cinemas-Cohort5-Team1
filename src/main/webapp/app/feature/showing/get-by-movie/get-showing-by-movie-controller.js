ef70e5a9fd51b3db5c57caa8952722a1205ce645
omar-f <o.fahmy@outlook.com>
2017-03-20 15:05:59 +0000

    QA-181 #comment Added drop-down menu to movie booking page, and updated dynamic price

--- src/main/webapp/app/feature/showing/get-by-movie/get-showing-by-movie-controller.js
+++ src/main/webapp/app/feature/showing/get-by-movie/get-showing-by-movie-controller.js
@@ -1,8 +1,13 @@
 (function() {
 	
-	var GetShowingByMovieController = function(Auth, showingDal) {
+	var GetShowingByMovieController = function(Auth, showingDal, ticketDal) {
 		
 		var vm = this;
+        vm.globalAdultPrice = 0;
+        vm.globalChildPrice = 0;
+        vm.tempAdultTickets = 0;
+        vm.tempChildTickets = 0;
+        vm.totalPrice = 0;
 		
 		vm.getByMovie = function(movieId) {
 			showingDal.getShowingByMovie(movieId).then(function(results) {
@@ -21,12 +26,55 @@
                 $('#hiddenShowingId').val(showing.showingId);
                 $('#hiddenShowingId').trigger('input');
                 Auth.setShowingId(showing.showingId);
+                vm.updateGlobalPrices(showing.showingId);
             };
 
 
 		}
 
 
+        vm.updateAdultQty = function (qty) {
+            if (qty < 0) {
+                return;
+            }
+
+            vm.tempAdultTickets = qty;
+            vm.updatePrice();
+        };
+
+        vm.updateChildQty = function (qty) {
+            if (qty < 0) {
+                return;
+            }
+
+            vm.tempChildTickets = qty;
+            vm.updatePrice();
+        };
+
+        vm.updatePrice = function () {
+            vm.totalPrice = parseFloat(vm.tempAdultTickets * vm.globalAdultPrice) + parseFloat(vm.tempChildTickets * vm.globalChildPrice);
+        };
+
+
+        vm.updateGlobalPrices = function (showingId) {
+            ticketDal.getPrice(showingId, 'ADULT').then(function (result) {
+                vm.globalAdultPrice = result.price;
+                vm.updatePrice();
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+
+            ticketDal.getPrice(showingId, 'CHILD').then(function (result) {
+                vm.globalChildPrice = result.price;
+                vm.updatePrice();
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+        }
+
+
 	};
-	angular.module("movieApp").controller("getShowingByMovieController", ["Auth", "showingDal", GetShowingByMovieController]);
+	angular.module("movieApp").controller("getShowingByMovieController", ["Auth", "showingDal", "ticketDal", GetShowingByMovieController]);
 }());
