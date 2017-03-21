be8448a1bb2e7d87faaaad47f340778e35f4f96c
haileymyers <hailey.r.myers@bath.edu>
2017-03-21 17:33:10 +0000

    QA-208 #comment mockito testing

---
+++ src/test/java/com/qa/cinema/util/PersistShowing.java
@@ -0,0 +1,26 @@
+package com.qa.cinema.util;
+
+import org.mockito.stubbing.Answer;
+import com.qa.cinema.persistence.Showing;
+
+import java.util.Collection;
+
+import org.mockito.invocation.InvocationOnMock;
+
+public class PersistShowing implements Answer<Void> {
+	
+	private Collection<Showing> showings;
+	
+	public PersistShowing(Collection<Showing> showings) {
+		this.showings = showings;
+	}
+	
+	@Override
+	public Void answer(InvocationOnMock invocation) throws Throwable {
+		Showing showingToPersist = (Showing) invocation.getArguments()[0];
+		showings.add(showingToPersist);
+		System.out.println(showingToPersist);
+		return null;
+	}
+
+}
